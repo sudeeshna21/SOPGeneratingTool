0541cf3d8ea5b34661e1ddd91db7e6cf3100d228
sudeeshna21 <sudeeshnareddy21@gmail.com>
2023-09-03 21:49:30 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const openAIConfig = {
-  OPENAI_API_KEY: "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",
+  OPENAI_API_KEY: "",
 };
 
 const brevoConfig = {
   url: "https://api.sendinblue.com/v3",
   apiKey:
-    "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",
+    "",
   sender: {
     senderName: "Team Effizient",
     senderEmail: "sinfo@effizient.ca",
